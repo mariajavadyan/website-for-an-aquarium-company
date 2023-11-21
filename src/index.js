4ba406107c3e6fbac3348aaad52e770d061dba3f
DeveloperSamvel <developer.vardanyan@gmail.com>
2023-11-21 20:33:01 +0400

    webpack initial commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./style.css";
+
+console.log("test");console.log("test2");console.log("test3");
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: path.resolve(__dirname, "src/index.js"),
+    },
+    output: {
+        filename: "[hash].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, "src/index.html"),
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+};
