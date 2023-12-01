685f1f7cd20fa7efbabe523c352a14e2e85e7858
Karo Kendimian <karo.kendimian@outlook.com>
2023-12-01 22:01:39 +0400

    current state: commit for team meating

--- src/components/MainContent/HomeSection/aquarium.js
+++ src/components/MainContent/HomeSection/aquarium.js
@@ -28,7 +28,10 @@ function move() {
     (destination.x > currentPosition.x && flipped)
   ) {
     clearInterval(motionIntervalId);
-    const flipIntervalId = setInterval(flip(), 1000);
+    flip();
+    setTimeout(() => {
+      motionIntervalId = setInterval(move, 4000);
+    }, 1000);
   } else {
     fish.style.paddingTop = `${destination.y}px`;
     fish.style.paddingLeft = `${destination.x}px`;
@@ -44,6 +47,44 @@ function flip() {
   flipped = !flipped;
   motionIntervalId = setInterval(move, 4000);
 }
+
+function swimAway() {
+  clearInterval(motionIntervalId);
+  currentPosition = getCurrentPosition();
+  const mousePosition = {
+    x: event.clientX - aquarium.offsetLeft,
+    y: event.clientY - aquarium.offsetTop,
+  };
+  const angle = Math.atan2(
+    mousePosition.y - currentPosition.y,
+    mousePosition.x - currentPosition.x
+  );
+  const oppositeAngle = angle + Math.PI;
+  const distance = Math.min(
+    aquarium.offsetWidth - currentPosition.x,
+    aquarium.offsetHeight - currentPosition.y,
+    currentPosition.x,
+    currentPosition.y
+  );
+  destination = {
+    x: currentPosition.x + distance * Math.cos(oppositeAngle),
+    y: currentPosition.y + distance * Math.sin(oppositeAngle),
+  };
+
+  if (
+    (destination.x < currentPosition.x && !flipped) ||
+    (destination.x > currentPosition.x && flipped)
+  ) {
+    flip();
+  }
+  fish.style.paddingTop = `${destination.y}px`;
+  fish.style.paddingLeft = `${destination.x}px`;
+  setTimeout(() => {
+    motionIntervalId = setInterval(move, 4000);
+  }, 5000);
+}
+
+fish.addEventListener("click", swimAway);
 
 move();
 const motionIntervalId = setInterval(move, 4000);
