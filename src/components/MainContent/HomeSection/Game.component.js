32f31c3bf6fdec5be7ad3213e7c832b00fc24acc
Maria Javadyan <maria.javadyan02@gmail.com>
2023-12-02 13:29:09 +0400

    Fixing Name

--- src/components/MainContent/HomeSection/Game.component.js
+++ src/components/MainContent/HomeSection/Game.component.js
@@ -2,29 +2,29 @@ import imgSrc from "@/static/images/aquarium8.png";
 import styles from "./Home.module.css";
 
 function createGame() {
-    const gameDiv = document.createElement("div");
-    const titleElement = document.createElement("p");
-    const gameDivElement = document.createElement("div");
-    const imgElement = document.createElement("img");
-    const gameElement = document.createElement("p");
+  const gameDiv = document.createElement("div");
+  const titleElement = document.createElement("p");
+  const gameDivElement = document.createElement("div");
+  const imgElement = document.createElement("img");
+  const gameElement = document.createElement("p");
 
-    gameDiv.classList.add(styles["gameContainer"]);
-    titleElement.classList.add(styles["title"]);
-    gameDivElement.classList.add(styles["gameSection"]);
-    imgElement.setAttribute("class", styles["acuarium"]);
-    imgElement.setAttribute("src", imgSrc);
-    gameElement.classList.add(styles["game"]);
+  gameDiv.classList.add(styles["gameContainer"]);
+  titleElement.classList.add(styles["title"]);
+  gameDivElement.classList.add(styles["gameSection"]);
+  imgElement.setAttribute("class", styles["acuarium"]);
+  imgElement.setAttribute("src", imgSrc);
+  gameElement.classList.add(styles["game"]);
 
-    titleElement.textContent = "AQUA DREAM SPACE";
-    gameElement.textContent = "Game";
+  titleElement.textContent = "AQUA DREAM SPACE";
+  // gameElement.textContent = "Game";
 
-    gameDivElement.appendChild(imgElement);
-    gameDivElement.appendChild(gameElement);
+  gameDivElement.appendChild(imgElement);
+  gameDivElement.appendChild(gameElement);
 
-    gameDiv.appendChild(titleElement);
-    gameDiv.appendChild(gameDivElement);
+  gameDiv.appendChild(titleElement);
+  gameDiv.appendChild(gameDivElement);
 
-    return gameDiv;
+  return gameDiv;
 }
-  
+
 export default createGame;
