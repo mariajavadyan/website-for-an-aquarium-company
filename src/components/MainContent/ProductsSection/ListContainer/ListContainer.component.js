358546a3538020c4feb8ad4dc90a91d8766bb442
Maria Javadyan <maria.javadyan02@gmail.com>
2023-12-07 20:30:13 +0400

    Adding Debaunce component in Search

--- src/components/MainContent/ProductsSection/ListContainer/ListContainer.component.js
+++ src/components/MainContent/ProductsSection/ListContainer/ListContainer.component.js
@@ -7,17 +7,17 @@ function CreateListContainer() {
   productListContainer.id = "productList";
   productListContainer.classList.add(styles["product-container"]);
 
-  let productId = document.getElementById('Products');
+  let productId = document.getElementById("Products");
   productId.addEventListener("click", function () {
-
-    displayProducts().then((products) => {
-      for (const product of products) {
-        productListContainer.appendChild(Product(product));
-      }
-    })
-    .catch((error) => {
-      console.error('Ошибка при загрузке изображения:', error);
-    });
+    displayProducts()
+      .then((products) => {
+        for (const product of products) {
+          productListContainer.appendChild(Product(product));
+        }
+      })
+      .catch((error) => {
+        console.error("Error loading images: ", error);
+      });
   });
 
   return productListContainer;
--- src/components/MainContent/ProductsSection/SearchContainer/Search.component.js
+++ src/components/MainContent/ProductsSection/SearchContainer/Search.component.js
@@ -1,7 +1,66 @@
 import displayProducts from "@/services/displayProducts";
 import Product from "../Product/Product.component";
 import searchProducts from "@/services/searchProducts";
+import Debounce from "./Debounce.component";
 import styles from "./Search.module.css";
+
+// function debounce(func, wait) {
+//   let timeout;
+
+//   return function executedFunction(...args) {
+//     const later = () => {
+//       clearTimeout(timeout);
+//       func(...args);
+//     };
+
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//   };
+// }
+
+// function CreateSearch() {
+//   const searchContainer = document.createElement("div");
+//   searchContainer.id = "searchContainer";
+//   searchContainer.classList.add(styles["searchContainer"]);
+
+//   const label = document.createElement("label");
+//   label.for = "searchBar";
+//   label.textContent = "Search: ";
+
+//   const searchBar = document.createElement("input");
+//   searchBar.type = "text";
+//   searchBar.id = "searchBar";
+//   searchBar.classList.add(styles["searchBar"]);
+//   searchBar.oninput = function () {
+//     let productListContainer = document.querySelector("#productList");
+//     productListContainer.innerHTML = "";
+
+//     displayProducts(searchProducts())
+//       .then((products) => {
+//         const searchInput = document
+//           .getElementById("searchBar")
+//           .value.toLowerCase();
+//         const filteredProducts = products.filter((product) => {
+//           return (
+//             product.name.toLowerCase().includes(searchInput) ||
+//             product.description.toLowerCase().includes(searchInput)
+//           );
+//         });
+
+//         filteredProducts.forEach((product) => {
+//           productListContainer.appendChild(Product(product));
+//         });
+//       })
+//       .catch((error) => {
+//         console.error("Error loading images: ", error);
+//       });
+//   };
+
+//   searchContainer.appendChild(label);
+//   searchContainer.appendChild(searchBar);
+
+//   return searchContainer;
+// }
 
 function CreateSearch() {
   const searchContainer = document.createElement("div");
@@ -16,27 +75,33 @@ function CreateSearch() {
   searchBar.type = "text";
   searchBar.id = "searchBar";
   searchBar.classList.add(styles["searchBar"]);
-  searchBar.oninput = function () {
+
+  const debouncedSearch = Debounce(function () {
     let productListContainer = document.querySelector("#productList");
     productListContainer.innerHTML = "";
 
-    displayProducts(searchProducts()).then(products => {
-      const searchInput = document.getElementById("searchBar").value.toLowerCase();
-      const filteredProducts = products.filter((product) => {
-        return (
-          product.name.toLowerCase().includes(searchInput) ||
-          product.description.toLowerCase().includes(searchInput)
-        );
-      });
+    displayProducts(searchProducts())
+      .then((products) => {
+        const searchInput = document
+          .getElementById("searchBar")
+          .value.toLowerCase();
+        const filteredProducts = products.filter((product) => {
+          return (
+            product.name.toLowerCase().includes(searchInput) ||
+            product.description.toLowerCase().includes(searchInput)
+          );
+        });
 
-      filteredProducts.forEach(product => {
-        productListContainer.appendChild(Product(product));
+        filteredProducts.forEach((product) => {
+          productListContainer.appendChild(Product(product));
+        });
+      })
+      .catch((error) => {
+        console.error("Error loading images: ", error);
       });
-    })
-    .catch((error) => {
-      console.error('Ошибка при загрузке изображения:', error);
-    });
-  };
+  }, 500);
+
+  searchBar.oninput = debouncedSearch;
 
   searchContainer.appendChild(label);
   searchContainer.appendChild(searchBar);
