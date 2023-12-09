06081ce3e38f7eb06f0f27f35f8ccc23f303e906
Maria Javadyan <maria.javadyan02@gmail.com>
2023-12-09 20:27:47 +0400

    Deleting comments

--- src/helpers/sendEmail.js
+++ src/helpers/sendEmail.js
@@ -8,7 +8,7 @@ function sendEmail() {
     message: document.getElementById("message").value,
   };
   emailjs
-    .send("service_0i4h0pk", "template_cdoya2p", params, "MQtLIIqwQH_67LOBO")
+    .send("service_pzog1ft", "template_cdoya2p", params, "MQtLIIqwQH_67LOBO")
     .then(function (res) {
       alert("Success! " + res.status);
     });
